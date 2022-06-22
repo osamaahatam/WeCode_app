7e0bb0aa0f77d9f1783eb427c043bf818193f6de
osama <osama.hatam01@gmail.com>
2022-06-22 14:26:44 +0300

    adding slider to the application

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 // import 'package:wecode/src/my_app.dart';
 
@@ -10,49 +11,102 @@ void main() {
 }
 
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+List<String> _images=[
+'assets/image1.jpg',
+'assets/image2.jpg' , 
+'assets/image3.jpg',
+];
+String _temp=_images[0];
+
+class _HomeState extends State<Home> {
+
 
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       backgroundColor:const  Color.fromARGB(255, 68, 68, 68),
       appBar: AppBar(
-        title: const Center(child: Text('WE CODE')),
+        title:const  Text('WE CODE'),
+        centerTitle: true,
         backgroundColor: const Color.fromARGB(255, 68, 68, 68),
       ),
       body: Center(
         child: Column(
           //to center the colunm in the body
-          mainAxisAlignment: MainAxisAlignment.center,
+          //mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(50),
-              child: Image.network(
-                "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/53ad14af-0dd4-4715-882a-da418cc0e544/d7qpsq5-12eb78c3-6b19-430b-92e2-07000b4b5db0.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwic3ViIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImF1ZCI6WyJ1cm46c2VydmljZTpmaWxlLmRvd25sb2FkIl0sIm9iaiI6W1t7InBhdGgiOiIvZi81M2FkMTRhZi0wZGQ0LTQ3MTUtODgyYS1kYTQxOGNjMGU1NDQvZDdxcHNxNS0xMmViNzhjMy02YjE5LTQzMGItOTJlMi0wNzAwMGI0YjVkYjAuanBnIn1dXX0.QpQ8CRU0nXPIOZ8Y5V9jOji3dseNr0NiDzKHnRw56mU",
-                width: 300.0,
-                height: 250.0,
-                ),
-            ),
-            const SizedBox(height: 20.0),
-            const Text(
-              'Hello Kurdistan',
-               style: TextStyle(
-                shadows: [
-                  Shadow(
-                   blurRadius: 10.0,
-                   offset: Offset(5, 5)
-                  ),
-                ],
-               fontSize: 30.0,
-               color: Colors.white,
-               ),
+          children:  [
+            const SizedBox(height: 40.0,),
+             const Text(
+              'Choose The city',
+              style: TextStyle(
+                fontSize:30.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
              ),
-          ],
-        ),
+             ClipRRect(
+              //the main image 
+              child:Image.asset(
+                _temp,
+                height: 250,
+                width: 350,
+                ),
+              // borderRadius: BorderRadius.all(),
+             ),
+             Row(
+              children: [
+                const SizedBox(width: 25.0,height: 150.0,),
+                GestureDetector (
+                  child: Image.asset(
+                    _images[0],
+                    width: 100,
+                    height: 100,
+                    ), 
+                   onTap: (){
+                    setState(() {
+                      _temp=_images[0];
+                    });
+                  },
+                ),
+                const SizedBox(width: 5.0,),
+                GestureDetector (
+                  child: Image.asset(
+                    _images[1],
+                    width: 135,
+                    height: 100,
+                    ), 
+                   onTap: (){
+                    setState(() {
+                      _temp=_images[1];
+                    });
+                  },
+                ),
+              const SizedBox(width: 5.0,),
+                GestureDetector (
+                  child: Image.asset(
+                    _images[2],
+                    width: 100,
+                    height: 120,
+                    ), 
+                   onTap: (){
+                    setState(() {
+                      _temp=_images[2];
+                    });
+                  },
+                ),
+              ],
+             ),
+           ],
           ),
-    );
+        ),
+          );
   }
 }
 
